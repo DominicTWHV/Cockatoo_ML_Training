82458019972fe3e6278de6ae975f6738d145ef26
DominicTWHV <dominic@twhvyt.com>
2026-02-12 04:59:58 -0700

    Fixed blocking issue in training pipeline script

--- start_train.sh
+++ start_train.sh
@@ -4,5 +4,7 @@ python3 test_gpu.py
 python3 download_data.py
 python3 prepare_data.py
 
-screen -mS train_text bash -lc 'cd /home/dominic/orion && source venv/bin/activate && python3 train_text.py'
+screen -dmS train_text bash -lc 'cd /home/dominic/orion && source venv/bin/activate && python3 train_text.py'
 screen -dmS tensorboard bash -lc 'cd /home/dominic/orion && source venv/bin/activate && tensorboard --logdir=constellation_one_text --host=0.0.0.0 --port=6007'
+
+screen -r train_text
