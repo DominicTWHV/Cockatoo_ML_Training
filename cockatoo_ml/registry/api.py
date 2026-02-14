214860ea94bf25298049a5dc9b715ed50707d224
DominicTWHV <dominic@twhvyt.com>
2026-02-14 02:53:16 -0700

    Revert
    this reverts commit 964a5d23137cef6ec98c332eb92ff626c1f50e5e.

--- cockatoo_ml/registry/api.py
+++ cockatoo_ml/registry/api.py
@@ -24,16 +24,16 @@ class APIConfig:
 
 class WebhookConfig:
     # enable training telemetry
-    enable = True
+    enable = False
 
     # enable validation telemetry (sent to a separate endpoint)
-    enable_validation = True
+    enable_validation = False
 
     # default POST endpoint to send training telemetry to
-    DEFAULT_WEBHOOK_URL = "https://api.cockatoo.dev/api/training/data"
+    DEFAULT_WEBHOOK_URL = "https://yourdomain.com/<path>"
 
     # default POST endpoint to send validation telemetry to
-    DEFAULT_VALIDATION_WEBHOOK_URL = "https://api.cockatoo.dev/api/evaluation/data"
+    DEFAULT_VALIDATION_WEBHOOK_URL = "https://yourdomain.com/<validation-path>"
     
     @classmethod
     def get_webhook_url(cls):
