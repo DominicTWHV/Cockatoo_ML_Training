4b41ce1e3e57a9e5447de3e0f0504cf8aa5cac8b
DominicTWHV <dominic@twhvyt.com>
2026-02-17 20:39:57 -0700

    Modified screen name from train_text to training

--- start_train.sh
+++ start_train.sh
@@ -4,7 +4,7 @@ python3 test_gpu.py
 python3 download_data.py
 python3 prepare_data.py
 
-screen -dmS train_text bash -lc 'cd /home/dominic/orion && source venv/bin/activate && python3 train.py'
+screen -dmS training bash -lc 'cd /home/dominic/orion && source venv/bin/activate && python3 train.py'
 screen -dmS tensorboard bash -lc 'cd /home/dominic/orion && source venv/bin/activate && tensorboard --logdir=constellation_one_text --host=0.0.0.0 --port=6007'
 
-screen -r train_text
+screen -r training
